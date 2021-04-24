41675d98ebaed892f4f52f7fa9be5773b0b56db9
ditam <ditam@users.noreply.github.com>
2021-04-24 16:25:31 +0200

    add base card styling

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 
+const decks = {
+  shaft: [{}],
+  playerHands: []
+};
+
+let playArea;
+
+function createCard(x, y, faceUp) {
+
+  const container = $('<div>').addClass('card').toggleClass('face-down', !faceUp);
+  container.css({
+    left: x,
+    top: y
+  });
+
+  container.appendTo(playArea);
+
+  // create card in DOM
+  // put card in a deck
+}
+
 $(document).ready(function() {
   console.log('Hello Mineshaft!');
+
+  playArea = $('#play-area');
+
+  createCard(200, 200, true);
+  createCard(550, 250, false);
 });

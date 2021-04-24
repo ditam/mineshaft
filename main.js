0f54b39da67ab82e81618804201cea6e038511d4
ditam <ditam@users.noreply.github.com>
2021-04-24 21:08:41 +0200

    add card structure and assets

--- main.js
+++ main.js
@@ -12,7 +12,59 @@ const decks ={
 
 let playArea;
 
-function createCard(x, y, faceUp) {
+// utils -- TODO: move to separate file
+function getRandomItem(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function preloadImage(url)
+{
+  const img = new Image();
+  img.src = url;
+  return img;
+}
+// end utils
+
+const toolCardTypes = {
+  'pickaxe': {
+    assetURL: 'assets/pickaxe.png',
+    cost: 0,
+    description: 'Can be used to whatever or I guess also once I figure it out something something.',
+    displayName: 'Pickaxe'
+  },
+  'tnt': {
+    assetURL: 'assets/tnt.png',
+    cost: 0,
+    description: 'Boom-boom.',
+    displayName: 'T.N.T.'
+  }
+};
+
+const shaftCardTypes = {
+  'soil': {
+    assetURL: 'assets/soil.png',
+    cost: 0,
+    description: '',
+    displayName: 'Soil'
+  },
+  'rock': {
+    assetURL: 'assets/rock.png',
+    cost: 0,
+    description: '',
+    displayName: 'Rock'
+  }
+};
+
+function getCardType(type) {
+  if (type in toolCardTypes) {
+    return toolCardTypes[type];
+  } else {
+    return shaftCardTypes[type];
+  }
+}
+
+function createCard(type, x, y, faceUp) {
+  console.assert(type in toolCardTypes || type in shaftCardTypes);
 
   const container = $('<div>').addClass('card').css({
     left: x,
@@ -23,13 +75,30 @@ function createCard(x, y, faceUp) {
   // for now, every face-down card is passive, but we might change this...
   container.toggleClass('passive', !faceUp);
 
-  container.data('type', 'test-type');
+  if (faceUp) {
+    cardType = getCardType(type);
+    $('<div>').addClass('cost').text(cardType.cost).appendTo(container);
+    $('<div>').addClass('header').text(cardType.displayName).appendTo(container);
+    $('<img>').attr('src', cardType.assetURL).appendTo(container);
+    $('<div>').addClass('description').text(cardType.description).appendTo(container);
+  }
+
+  container.data('type', type);
 
   container.appendTo(playArea);
 
-  // create card in DOM
-  // put card in a deck
+  // TODO: put card in a deck
 }
+
+(function init() {
+  // preload card images
+  for (const [key, type] of Object.entries(toolCardTypes)) {
+    type.img = preloadImage(type.assetURL);
+  }
+  for (const [key, type] of Object.entries(shaftCardTypes)) {
+    type.img = preloadImage(type.assetURL);
+  }
+})();
 
 $(document).ready(function() {
   console.log('Hello Mineshaft!');
@@ -40,8 +109,9 @@ $(document).ready(function() {
     console.log('card click, type:', $(this).data('type'));
   });
 
-  createCard(200, 500, true);
-  createCard(420, 500, true);
-  createCard(640, 500, true);
-  createCard(decks.shaft.position.x, decks.shaft.position.y, false);
+
+  createCard('pickaxe', 200, 200, true);
+  createCard('tnt', 420, 200, true);
+  createCard('pickaxe', 640, 500, true);
+  createCard('soil', decks.shaft.position.x, decks.shaft.position.y, false);
 });
